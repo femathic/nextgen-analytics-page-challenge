29ca45f58de300e9e8f106eba47accc77b63e5e2
Femi Opanuga <femathic@gmail.com>
2020-06-11 22:44:01 +0100

    Updates

---
+++ precache-manifest.3eb066e1f7ef1936ff3e98de42c11228.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14441c538161c81ebf0207e756e2c588",
+    "url": "/nextgen-analytics-page-challenge/index.html"
+  },
+  {
+    "revision": "2b31c3df0e33fac9105d",
+    "url": "/nextgen-analytics-page-challenge/static/css/main.38349444.chunk.css"
+  },
+  {
+    "revision": "025c6fcf26bc8140106c",
+    "url": "/nextgen-analytics-page-challenge/static/js/2.79e5112c.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/nextgen-analytics-page-challenge/static/js/2.79e5112c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b31c3df0e33fac9105d",
+    "url": "/nextgen-analytics-page-challenge/static/js/main.0d8bdaf3.chunk.js"
+  },
+  {
+    "revision": "933a1cb8eca654ad8417",
+    "url": "/nextgen-analytics-page-challenge/static/js/runtime-main.f66e1d55.js"
+  },
+  {
+    "revision": "9d5abe024c803a227c58895af5b74d86",
+    "url": "/nextgen-analytics-page-challenge/static/media/Group174.9d5abe02.svg"
+  },
+  {
+    "revision": "4d75dec6ee1dea66193887beb61ea6eb",
+    "url": "/nextgen-analytics-page-challenge/static/media/Path.4d75dec6.svg"
+  },
+  {
+    "revision": "ae3b850d3afb7bf21234e29fd7be7137",
+    "url": "/nextgen-analytics-page-challenge/static/media/avatar@2x.ae3b850d.png"
+  },
+  {
+    "revision": "77dd184a422b10d33f2404af5efeb8f3",
+    "url": "/nextgen-analytics-page-challenge/static/media/funds.77dd184a.svg"
+  },
+  {
+    "revision": "d333eba63e2168e328a7fc11e3a5a5aa",
+    "url": "/nextgen-analytics-page-challenge/static/media/history.d333eba6.svg"
+  },
+  {
+    "revision": "e870ff40e41dc156bbe2881fdd870279",
+    "url": "/nextgen-analytics-page-challenge/static/media/locked.e870ff40.svg"
+  },
+  {
+    "revision": "37c1d7ebca2705cfd4a0b7940dfad524",
+    "url": "/nextgen-analytics-page-challenge/static/media/verification-window-button.37c1d7eb.svg"
+  }
+]);
